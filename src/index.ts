aee12db5a14a94e48b895990a3b5da77a9c21782
dimaslanjaka <dimaslanjaka@gmail.com>
2026-02-10 21:09:11 +0700

    fix: update import paths to include file extensions for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,55 @@
-export { default as Database, default } from './database';
-export { default as Document } from './document';
-export { default as Model } from './model';
-export { default as Mutex } from './mutex';
-export { default as Query } from './query';
-export { default as Schema } from './schema';
-export { default as SchemaType } from './schematype';
+export { default as Database, default } from './database.js';
+export { default as Document } from './document.js';
+export { default as Model } from './model.js';
+export { default as Mutex } from './mutex.js';
+export { default as Query } from './query.js';
+export { default as Schema } from './schema.js';
+export { default as SchemaType } from './schematype.js';
 export type {
-  AddSchemaTypeLoopOptions, AddSchemaTypeMixedOptions, AddSchemaTypeOptions, AddSchemaTypeSimpleOptions, NodeJSLikeCallback,
-  Options, queryCallback, queryFilterCallback, queryParseCallback, SchemaTypeOptions
-} from './types';
-export { default as SchemaTypeArray } from './types/array';
-export { default as SchemaTypeBoolean } from './types/boolean';
-export { default as SchemaTypeBuffer } from './types/buffer';
-export { default as SchemaTypeCUID } from './types/cuid';
-export { default as SchemaTypeDate } from './types/date';
-export { default as SchemaTypeEnum } from './types/enum';
+  AddSchemaTypeLoopOptions,
+  AddSchemaTypeMixedOptions,
+  AddSchemaTypeOptions,
+  AddSchemaTypeSimpleOptions,
+  NodeJSLikeCallback,
+  Options,
+  queryCallback,
+  queryFilterCallback,
+  queryParseCallback,
+  SchemaTypeOptions
+} from './types.js';
+export { default as SchemaTypeArray } from './types/array.js';
+export { default as SchemaTypeBoolean } from './types/boolean.js';
+export { default as SchemaTypeBuffer } from './types/buffer.js';
+export { default as SchemaTypeCUID } from './types/cuid.js';
+export { default as SchemaTypeDate } from './types/date.js';
+export { default as SchemaTypeEnum } from './types/enum.js';
 export {
-  Array, Boolean, Buffer, CUID, Date, Enum,
-  Integer, Mixed, Number, Object, String, Virtual
-} from './types/index';
-export { default as SchemaTypeInteger } from './types/integer';
-export { default as SchemaTypeNumber } from './types/number';
-export { default as SchemaTypeObject } from './types/object';
-export { default as SchemaTypeString } from './types/string';
-export { default as SchemaTypeVirtual } from './types/virtual';
-export { arr2obj, asyncWriteToStream, delProp, getProp, parseArgs, reverse, setGetter, setProp, shuffle } from './util';
-
+  Array,
+  Boolean,
+  Buffer,
+  CUID,
+  Date,
+  Enum,
+  Integer,
+  Mixed,
+  Number,
+  Object,
+  String,
+  Virtual
+} from './types/index.js';
+export { default as SchemaTypeInteger } from './types/integer.js';
+export { default as SchemaTypeNumber } from './types/number.js';
+export { default as SchemaTypeObject } from './types/object.js';
+export { default as SchemaTypeString } from './types/string.js';
+export { default as SchemaTypeVirtual } from './types/virtual.js';
+export {
+  arr2obj,
+  asyncWriteToStream,
+  delProp,
+  getProp,
+  parseArgs,
+  reverse,
+  setGetter,
+  setProp,
+  shuffle
+} from './util.js';
